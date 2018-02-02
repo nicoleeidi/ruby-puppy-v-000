0715c66225077ef4f3501200a3b250ef4e5570b3
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-02 22:13:32 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class Dog
+  @@all= []
+  attr_accessor :name
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+  def self.all
+    @@all.each do |instance|
+      puts instance.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
